5cf0e9d32c136b6a9df24c27c592a1032651a968
Andrew Wray <andrewwray2718@gmail.com>
2021-03-03 21:40:04 -0800

    Update README and script with more example code

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # rg_evaluation
 Research Group - Evaluation
+
+## PyMySQL rundown
+
+The two main objects you want to work with in PyMySQL are 1) the database connection object (which is `db` in the script) and 2) the `cursor` object. A typical workflow looks like this:
+
+    # set up the database connection 
+    db = pymysql.connect(...database info...)
+
+    # pull out the cursor from the connection (only need to do this once like this)
+    cursor = db.cursor()
+
+    # write a sql query as a python string
+    query = """SELECT * FROM table"""
+
+    # tell the cursor to execute the query
+    cursor.execute(query)
+
+    # grab the results of the query and store it in
+    # a python object that I called "data"
+    data = cursor.fetchall()
+
+    # do what you want with data, like print it.
+    print(data)
+
+
+Optionally, if you want to make changes to the database, the workflow is similar, but you need to commit the changes:
+
+    # sql query to do some stuff
+    query = """ INSERT INTO table VALUES (blah blah blah)"""
+
+    # execute the query (this alters the local model of the database)
+    cursor.execute(query)
+
+    # send the changes to the cloud on AWS
+    db.commit()
--- sofaidb.py
+++ sofaidb.py
@@ -1,30 +1,54 @@
 # Access AWS with Python 3
 
 # db: sofai-mysql
-# username: admin 
+# username: admin
 # password: soundofai
 # port: 3306
 # hostname: sofai-mysql.cbyywm0dtzft.us-east-2.rds.amazonaws.com
 
 import pymysql
 
-db = pymysql.connect(host= 'sofai-mysql.cbyywm0dtzft.us-east-2.rds.amazonaws.com', 
-        user='admin', password='soundofai')
+db = pymysql.connect(host='sofai-mysql.cbyywm0dtzft.us-east-2.rds.amazonaws.com',
+                     user='admin', password='soundofai')
 
 cursor = db.cursor()
 
+# this is just a python object; just prints the location of the object in your memory
 print(cursor)
 
+# get the sql version of the database
 cursor.execute("select version()")
 data = cursor.fetchone()
-
 print(data)
 
-# Initially Create Table
+# Initially create the database; only run once
 # sql = '''create database SofAISurvey'''
 # cursor.execute(sql)
 
+# gets the tables. This was empty until a table was created.
 cursor.execute('''use SofAISurvey''')
 cursor.execute('''show tables''')
-cursor.fetchall()
+data = cursor.fetchall()
+print(data)
 
+# create a table. Only uncomment this if you want to create the table again!!!
+# cursor.execute('''CREATE TABLE survey1 (question INT)''')
+
+# add a question with the label "1" to the table named survey1
+# insert_query = """
+#         INSERT INTO survey1
+#         VALUES (17)
+#         """
+# cursor.execute(insert_query)
+
+select_all_query = """
+                SELECT * FROM survey1
+                """
+cursor.execute(select_all_query)
+data = cursor.fetchall()
+print(data)
+
+
+# if you don't commit after running SQL queries, the database on the AWS server doesn't update.
+# only uncomment next line if you want to push changes.
+# db.commit()
