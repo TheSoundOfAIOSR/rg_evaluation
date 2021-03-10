47b063a7b77296d1a16db4f763be7ee432ab78ba
ckchmod <chris.kang@wsu.edu>
2021-03-10 07:28:31 -0800

    Balaji edit

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+"""
+Modified by Balaji - 9/3/2021
+DB operations added
+"""
+
 from flask import Flask, render_template, request
 from surveydata import SurveyData
+from sofaidb import DBInterface
+from datetime import datetime
 import os
 
 app = Flask(__name__)
 survey_data = SurveyData()
-filename = 'data.txt'
+filename = 'data.txt'	# This can be removed later
 
 @app.route('/')
 def root():
@@ -20,12 +27,31 @@ def surveyresponse():
     Handle Survey Response
     :return:
     """
-    response = request.args.get('field')
-
-    out = open(filename, 'a')
-    out.write(response + '\n')
-    out.close()
-    return render_template('thankyou.html', data=survey_data.data)
+    RspType = -1
+    rating = request.args.get('field')
+    
+    if rating: # equivalent of is None
+    	out = open(filename, 'a')
+    	out.write(rating + '\n')
+    	out.close()
+    	
+    	RspType = 1  # set it for radio button if at least one radio button is present in the questionnaire
+    	# Write ResponseInt <-- rating
+    
+    response = request.args.get('first_name')
+    if response:
+    	if RspType == -1:
+    	    RspType = 2	  # set it for text input. As of now, this field and Responder filed are one and the same.
+    	    # Write ResponseText <-- response
+    	    # Write Responder <-- response
+    	    # Write ResponseType <-- RspType
+    	    
+    CurrDate = datetime.now()
+    # Write ResponseDate <-- CurrDate
+    CurrDate = CurrDate.strftime("%Y-%m-%d")
+    DBInterface.writeToDB(RespUser='dummyUser', RespType=1, RespInt=int(rating), RespText=response, RespDate=CurrDate)
+    
+    return render_template('thankyou.html', data=survey_data.data, msg='data written to DB')
 
 @app.route('/results')
 def show_results():
@@ -46,5 +72,3 @@ def show_results():
 
 if __name__ == "__main__":
     app.run()
-
-
--- sofaidb.py
+++ sofaidb.py
@@ -1,3 +1,8 @@
+# Modified by Balaji - 9/3/2021
+# DB operations added
+# Creation script is commented out here. But was run separately once
+
+
 # Access AWS with Python 3
 
 # db: sofai-mysql
@@ -8,45 +13,56 @@
 
 import pymysql
 
-db = pymysql.connect(host='sofai-mysql.cbyywm0dtzft.us-east-2.rds.amazonaws.com',
-                     user='admin', password='soundofai')
+class DBInterface:
 
-cursor = db.cursor()
+    def writeToDB(RespUser, RespType, RespInt, RespText, RespDate):
+        db = pymysql.connect(host='sofai-mysql.cbyywm0dtzft.us-east-2.rds.amazonaws.com',
+                         user='admin', password='soundofai')
+        cursor = db.cursor()
+	
+        # this is just a python object; just prints the location of the object in your memory
+        print(cursor)
 
-# this is just a python object; just prints the location of the object in your memory
-print(cursor)
+        # get the sql version of the database
+        cursor.execute("select version()")
+        data = cursor.fetchone()
+        print(data)
 
-# get the sql version of the database
-cursor.execute("select version()")
-data = cursor.fetchone()
-print(data)
+        # Initially create the database; only run once
+        # sql = '''create database SofAISurvey'''
+        # cursor.execute(sql)
 
-# Initially create the database; only run once
-# sql = '''create database SofAISurvey'''
-# cursor.execute(sql)
+        # gets the tables. This was empty until a table was created.
+        cursor.execute('''use SofAISurvey''')
+        cursor.execute('''show tables''')
+        data = cursor.fetchall()
+        #print(data)
 
-# gets the tables. This was empty until a table was created.
-cursor.execute('''use SofAISurvey''')
-cursor.execute('''show tables''')
-data = cursor.fetchall()
-print(data)
+        # create a table. Only uncomment this if you want to create the table again!!!
+        # Primary key missing - add after discussions
+        #cursor.execute('''CREATE TABLE survey2 (questionNo int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        # 					Responder varchar(40) NOT NULL,
+        # 					ResponseType int,	#1 - for radio btn, 2 - for text, ...
+        # 					ResponseInt int,
+        # 					ResponseText varchar(40),
+        # 					ResponseDate	DATE )''')
 
-# create a table. Only uncomment this if you want to create the table again!!!
-# cursor.execute('''CREATE TABLE survey1 (question INT)''')
+        #db.commit()
 
-# add a question with the label "1" to the table named survey1
-# insert_query = """
-#         INSERT INTO survey1
-#         VALUES (17)
-#         """
-# cursor.execute(insert_query)
-
-select_all_query = """
-                SELECT * FROM survey1
-                """
-cursor.execute(select_all_query)
-data = cursor.fetchall()
-print(data)
+        # add a question with the label "1" to the table named survey1
+        insert_query = "INSERT INTO survey2 (Responder, ResponseType, ResponseInt, ResponseText, ResponseDate) \
+                                   VALUES  (%s,%s,%s,%s,%s)"	#, (RespUser, RespType, RespInt, RespText, RespDate)
+        cursor.execute(insert_query, (RespUser, RespType, RespInt, RespText, RespDate))
+        db.commit()
+        
+        # Clean up the table
+        #cursor.execute("DELETE FROM survey2")
+        #db.commit()
+        
+        select_all_query = "SELECT * FROM survey2"
+        cursor.execute(select_all_query)
+        data = cursor.fetchall()
+        print(data)
 
 
 # if you don't commit after running SQL queries, the database on the AWS server doesn't update.
