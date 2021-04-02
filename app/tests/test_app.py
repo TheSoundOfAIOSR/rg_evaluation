9836a2951e8bd03d8248515167307d3a39cbc53f
Andrew Wray <andrewwray2718@gmail.com>
2021-04-02 16:26:29 -0700

    Reorganize pytests

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,9 +1,11 @@
-from main import app
+from app import app
 import pytest
+
 
 @pytest.fixture
 def client():
     return app.test_client()
+
 
 def test_home_page():
     """
@@ -12,12 +14,11 @@ def test_home_page():
     THEN check that the response is valid
     """
 
-    flask_app = app
-
-    with flask_app.test_client() as test_client:
+    with app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
         assert b"Sound of AI Survey Questions" in response.data
+
 
 def test_submit(client):
     """
@@ -26,12 +27,12 @@ def test_submit(client):
     THEN check whether request is sent
     """
 
-    response = client.post('/responsedata', \
-        data = {
-            'q1': '1',
-            'q2': '1',
-            'q3': '1',
-            'q4': 'I-will-probably-use-it',
-            'q5': 'sample_1',
-        })
+    response = client.post('/responsedata',
+                           data={
+                               'q1': '1',
+                               'q2': '1',
+                               'q3': '1',
+                               'q4': 'I-will-probably-use-it',
+                               'q5': 'sample_1',
+                           })
     assert response.status_code == 200
